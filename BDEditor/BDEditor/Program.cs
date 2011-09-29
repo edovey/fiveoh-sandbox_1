2de0f00888b831bd6125e99f9e0a553371b48d19
Liz Dovey <grubbycodemonkey@gmail.com>
2011-09-29 12:35:59 -0600

    Additional changes for project settings

--- BDEditor/BDEditor/Program.cs
+++ BDEditor/BDEditor/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace BDEditor
 {
@@ -15,7 +16,7 @@ namespace BDEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Presentation());
         }
     }
 }

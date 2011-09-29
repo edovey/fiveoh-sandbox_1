68f58b626ab6c2eb4d74eba0df27cea41e0909f1
Liz Dovey <grubbycodemonkey@gmail.com>
2011-09-29 15:22:56 -0600

    Add simple controls and properties to user control for Section

--- BDEditor/BDEditor/Views/BDSectionControl.Designer.cs
+++ BDEditor/BDEditor/Views/BDSectionControl.Designer.cs
@@ -28,10 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.lblSectionName = new System.Windows.Forms.Label();
+            this.tbSectionName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblSectionName
+            // 
+            this.lblSectionName.AutoSize = true;
+            this.lblSectionName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSectionName.Location = new System.Drawing.Point(14, 20);
+            this.lblSectionName.Name = "lblSectionName";
+            this.lblSectionName.Size = new System.Drawing.Size(89, 18);
+            this.lblSectionName.TabIndex = 0;
+            this.lblSectionName.Text = "Section Title";
+            // 
+            // tbSectionName
+            // 
+            this.tbSectionName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSectionName.Location = new System.Drawing.Point(17, 53);
+            this.tbSectionName.Name = "tbSectionName";
+            this.tbSectionName.Size = new System.Drawing.Size(475, 24);
+            this.tbSectionName.TabIndex = 1;
+            // 
+            // BDSectionControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tbSectionName);
+            this.Controls.Add(this.lblSectionName);
+            this.Name = "BDSectionControl";
+            this.Size = new System.Drawing.Size(568, 156);
+            this.Load += new System.EventHandler(this.BDSectionControl_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblSectionName;
+        private System.Windows.Forms.TextBox tbSectionName;
     }
 }
--- BDEditor/BDEditor/Views/BDSectionControl.cs
+++ BDEditor/BDEditor/Views/BDSectionControl.cs
@@ -6,14 +6,36 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using BDEditor.DataModel;
 
 namespace BDEditor.Views
 {
     public partial class BDSectionControl : UserControl
     {
+        private BDSection currentSection;
+
+        public BDSection CurrentSection
+        {
+            get
+            {
+                return currentSection;
+            }
+
+            set
+            {
+                currentSection = value;
+                tbSectionName.Text = currentSection.name;
+            }
+        }
+
         public BDSectionControl()
         {
             InitializeComponent();
+        }
+
+        private void BDSectionControl_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }

98655e67c840c51e447475723027d9249ade299d
Liz Dovey <grubbycodemonkey@gmail.com>
2011-09-29 16:38:56 -0600

    Add class to extend generated BDLinkedNote

---
+++ BDEditor/BDEditor/DataModel/BDLinkedNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.EntityClient;
+using System.Data.Objects;
+using System.Linq;
+using System.Text;
+
+namespace BDEditor.DataModel
+{
+    /// <summary>
+    /// Extension of generated BDLinkedNote
+    /// </summary>
+    public partial class BDLinkedNote
+    {
+    }
+}

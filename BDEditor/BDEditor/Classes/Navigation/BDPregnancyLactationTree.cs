626ee200d4d36552d586cdbbf59a20ab341b3b71
Karl Schulze <karl.schulze@mac.com>
2012-05-28 15:07:05 -0600

    +Preg & Lact layout variants

---
+++ BDEditor/BDEditor/Classes/Navigation/BDPregnancyLactationTree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+
+using BDEditor.Classes;
+using BDEditor.DataModel;
+
+namespace BDEditor.Classes.Navigation
+{
+    public class BDPregnancyLactationTree
+    {
+        private BDPregnancyLactationTree() { }
+
+
+    }
+}

1a0488fec3cff0ab522c694ed34ba0fe5db8a43f
Karl Schulze <karl.schulze@mac.com>
2012-07-25 11:45:55 -0600

    BDTextControl wireframe

---
+++ BDEditor/BDEditor/Views/BDTextControl.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace BDEditor.Views
+{
+    partial class BDTextControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.bToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.geToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.leToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.plusMinusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.degreeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.µToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sOneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trademarkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(354, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bToolStripMenuItem,
+            this.geToolStripMenuItem,
+            this.leToolStripMenuItem,
+            this.plusMinusToolStripMenuItem,
+            this.degreeToolStripMenuItem,
+            this.µToolStripMenuItem,
+            this.sOneToolStripMenuItem,
+            this.trademarkToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(71, 180);
+            this.contextMenuStrip1.Text = "Insert Symbol";
+            // 
+            // bToolStripMenuItem
+            // 
+            this.bToolStripMenuItem.Name = "bToolStripMenuItem";
+            this.bToolStripMenuItem.Size = new System.Drawing.Size(70, 22);
+            this.bToolStripMenuItem.Text = "ß";
+            this.bToolStripMenuItem.ToolTipText = "Insert ß";
+            // 
+            // geToolStripMenuItem
+            // 
+            this.geToolStripMenuItem.Name = "geToolStripMenuItem";
+            this.geToolStripMenuItem.Size = new System.Drawing.Size(70, 22);
+            this.geToolStripMenuItem.Text = "≥";
+            this.geToolStripMenuItem.ToolTipText = "Insert ≥";
+            // 
+            // leToolStripMenuItem
+            // 
+            this.leToolStripMenuItem.Name = "leToolStripMenuItem";
+            this.leToolStripMenuItem.Size = new System.Drawing.Size(70, 22);
+            this.leToolStripMenuItem.Text = "≤";
+            this.leToolStripMenuItem.ToolTipText = "Insert ≤";
+            // 
+            // plusMinusToolStripMenuItem
+            // 
+            this.plusMinusToolStripMenuItem.Name = "plusMinusToolStripMenuItem";
+            this.plusMinusToolStripMenuItem.Size = new System.Drawing.Size(70, 22);
+            this.plusMinusToolStripMenuItem.Text = "±";
+            this.plusMinusToolStripMenuItem.ToolTipText = "Insert ±";
+            // 
+            // degreeToolStripMenuItem
+            // 
+            this.degreeToolStripMenuItem.Name = "degreeToolStripMenuItem";
+            this.degreeToolStripMenuItem.Size = new System.Drawing.Size(70, 22);
+            this.degreeToolStripMenuItem.Text = "°";
+            this.degreeToolStripMenuItem.ToolTipText = "Insert °";
+            // 
+            // µToolStripMenuItem
+            // 
+            this.µToolStripMenuItem.Name = "µToolStripMenuItem";
+            this.µToolStripMenuItem.Size = new System.Drawing.Size(70, 22);
+            this.µToolStripMenuItem.Text = "µ";
+            // 
+            // sOneToolStripMenuItem
+            // 
+            this.sOneToolStripMenuItem.Name = "sOneToolStripMenuItem";
+            this.sOneToolStripMenuItem.Size = new System.Drawing.Size(70, 22);
+            this.sOneToolStripMenuItem.Text = "¹";
+            this.sOneToolStripMenuItem.ToolTipText = "Insert ¹";
+            // 
+            // trademarkToolStripMenuItem
+            // 
+            this.trademarkToolStripMenuItem.Name = "trademarkToolStripMenuItem";
+            this.trademarkToolStripMenuItem.Size = new System.Drawing.Size(70, 22);
+            this.trademarkToolStripMenuItem.Text = "®";
+            this.trademarkToolStripMenuItem.ToolTipText = "Insert ®";
+            // 
+            // BDTextControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.textBox1);
+            this.Name = "BDTextControl";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Size = new System.Drawing.Size(360, 25);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem bToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem geToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem leToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem plusMinusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem degreeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem µToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sOneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trademarkToolStripMenuItem;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
---
+++ BDEditor/BDEditor/Views/BDTextControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BDEditor.Views
+{
+    public partial class BDTextControl : UserControl
+    {
+        public BDTextControl()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+    }
+}

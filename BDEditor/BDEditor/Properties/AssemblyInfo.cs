63070b792c694afba8dcf0851a4e90e6b44f78ad
Karl Schulze <karl.schulze@mac.com>
2012-09-17 22:03:32 -0600

    v.1.6.15

--- BDEditor/BDEditor/Properties/AssemblyInfo.cs
+++ BDEditor/BDEditor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Saint Street")]
 [assembly: AssemblyProduct("BDEditor")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©  2011, 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.6.14.00000")]
-[assembly: AssemblyFileVersion("1.6.14.00000")]
+[assembly: AssemblyVersion("1.6.15.00000")]
+[assembly: AssemblyFileVersion("1.6.15.00000")]

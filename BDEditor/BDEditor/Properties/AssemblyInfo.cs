6bf2a67e8dda6e7b835789d52b0990e5692ae4aa
Karl Schulze <karl.schulze@tlatech.com>
2015-05-28 17:59:32 -0600

    Editor 1.6.79

--- BDEditor/BDEditor/Properties/AssemblyInfo.cs
+++ BDEditor/BDEditor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Saint Street")]
 [assembly: AssemblyProduct("BDEditor")]
-[assembly: AssemblyCopyright("Copyright © 2011 - 2014")]
+[assembly: AssemblyCopyright("Copyright © 2011 - 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.6.78.00000")]
-[assembly: AssemblyFileVersion("1.6.78.00000")]
+[assembly: AssemblyVersion("1.6.79.00000")]
+[assembly: AssemblyFileVersion("1.6.79.00000")]
